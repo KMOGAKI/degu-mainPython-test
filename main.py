80a5b315fe7c56ae2cd1f9b58275ee7ed5fb1ec5
KMOGAKI <47022201+KMOGAKI@users.noreply.github.com>
2020-01-21 11:28:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import degu
 import ujson
 import time
+import machine
 
 if __name__ == '__main__':
     reported = {'state':{'reported':{}}}
@@ -8,5 +9,8 @@ if __name__ == '__main__':
 
     while True:
         degu.update_shadow(ujson.dumps(reported))
+        if (degu.check_update()):
+            print("New script is comming! Restarting...")
+            machine.reset();
         print("Hello! I'm Bob.")
         time.sleep(2)
